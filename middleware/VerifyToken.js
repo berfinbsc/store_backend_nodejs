5e6944e943459fc101e2573b6d644f28db39f8a3
berfin7 <berfinbasci4701@gmail.com>
2024-01-21 02:39:38 +0300

    verify token

--- middleware/VerifyToken.js
+++ middleware/VerifyToken.js
@@ -8,19 +8,15 @@ const key=process.env.SECRET_KEY
 const VerifyToken = (req,res,next)=>{
 
    const authHeader=  req.header('Authorization');
-   console.log(authHeader)
     if(!authHeader)
     return res.status(401).json({error:'Access denied'})
     if(!authHeader.startsWith('Bearer '))
-    return res.status(401).json({error:'token dügün değil'})
+    return res.status(401).json({error:'token düzgün değil'})
 
-    console.log("fff....................."+authHeader)
-    const token = authHeader.split(' ')[1]
-console.log("polopolo................."+token)
-console.log("jojo...................."+key)
+
 
 try {
-    
+    const token = authHeader.split(' ')[1]
     const decoded=jwt.verify(token,key)
     if(decoded.exp<Date.now()/1000) return res.status(401).json({message:'Token has expired'})
     const {userId}=decoded
@@ -30,7 +26,7 @@ try {
     next()
 } catch (error) {
     res.status(401).json({error})
-    console.log("verify errorooooooo: "+ error)
+    console.log("verify error : "+ error)
 }
 
       
